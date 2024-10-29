8d5b575449d1d951fa7d58aad3a52a1b709fd374
rrmartinez240 <rafaelmart1433@gmail.com>
2024-10-28 20:51:37 -0500

    Crear endpoint 2

--- app/api/temperature/route.ts
+++ app/api/temperature/route.ts
@@ -2,14 +2,9 @@ import { db } from '@vercel/postgres';
 
 export async function GET() {
   try {
-    // Realizar la consulta para obtener los datos de la tabla NTC
-    const result = await db.sql`SELECT * FROM NTC`;
-    const temperatures = result.rows;
-
-    // Devolver los datos en formato JSON
+    const { rows: temperatures } = await db.sql`SELECT * FROM NTC ORDER BY timestamp DESC;`;
     return new Response(JSON.stringify(temperatures), { status: 200 });
   } catch (error) {
-    // Manejar errores y devolver un mensaje de error simple
-    return new Response('Error al obtener los datos', { status: 500 });
+    return new Response(JSON.stringify({ error: 'Error al obtener los datos.' }), { status: 500 });
   }
 }
