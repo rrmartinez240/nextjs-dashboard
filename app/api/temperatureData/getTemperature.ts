625ee9385a4c693a6c7d1379da1d0a4dc8581533
rrmartinez240 <rafaelmart1433@gmail.com>
2024-10-30 21:29:01 -0500

    Ultimo 2

--- app/api/temperatureData/getTemperature.ts
+++ app/api/temperatureData/getTemperature.ts
@@ -9,6 +9,7 @@ export async function GET() {
     const result = await client.sql`SELECT * FROM NTC ORDER BY timestamp DESC`;
     return new Response(JSON.stringify(result.rows), { status: 200 });
   } catch (error) {
+    console.error('Error fetching data:', error);
     return new Response(JSON.stringify({ error: 'Failed to fetch data' }), { status: 500 });
   } finally {
     client.release();
