ded224eb1d8c018f23ef49f62b6b0d3a6dd46533
rrmartinez240 <rafaelmart1433@gmail.com>
2024-11-19 17:10:37 -0500

    1

--- app/api/temperature/obtener/route.ts
+++ app/api/temperature/obtener/route.ts
@@ -1,18 +1,24 @@
-// app/api/temperature/obtener/route.ts
 import { NextResponse } from 'next/server';
-import { Pool } from 'pg';  // Importa la librería pg
+import { Pool } from 'pg';
 
-// Carga la configuración de conexión desde el archivo .env
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL || "postgres://default:4GTVasStJo9U@ep-autumn-base-a4hibfh7-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
 });
 
 export async function GET() {
   try {
-    // Realiza la consulta a la base de datos
     const res = await pool.query('SELECT * FROM ntc');
-    return NextResponse.json(res.rows);
-  } catch (error: any) { // Captura cualquier error
+
+    // Crea la respuesta con los encabezados CORS
+    const response = NextResponse.json(res.rows);
+
+    // Agrega los encabezados CORS
+    response.headers.set('Access-Control-Allow-Origin', '*'); // Permite solicitudes desde cualquier dominio
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos permitidos
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type'); // Encabezados permitidos
+
+    return response;
+  } catch (error: any) {
     console.error('Error al recuperar datos:', error);
     return NextResponse.json({
       error: error.message,
